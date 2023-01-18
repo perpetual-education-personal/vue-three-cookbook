a700765ab9a76eb4e4fd52e0dc2c5246abb8751d
sheriffderek <derek@sheriffderek.consulting>
2023-01-18 10:26:20 -0800

    Add examples of ref and reactive lists and item removal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <script setup>
+   import { ref, reactive } from 'vue';
    import { RouterLink, RouterView } from 'vue-router';
    import HelloWorld from './components/HelloWorld.vue';
    import ProjectLogo from './components/graphics/ProjectLogo.vue';
+
+   const exampleData = [
+      {
+         id: 'a',
+         name: 'Andy',
+      },
+      {
+         id: 'b',
+         name: 'Burooj',
+      },
+      {
+         id: 'j',
+         name: 'Jesse',
+      },
+      {
+         id: 'n',
+         name: 'Ned',
+      },
+   ];
+
+   // ref version
+   const liveDataRef = ref(exampleData);
+
+   function removeItem(id) {
+      liveDataRef.value = liveDataRef.value.filter((item) => item.id !== id);
+   }
+
+   // reactive object version
+   const liveData = reactive({
+      list: exampleData,
+   });
+
+   function removeOtherItem(id) {
+      liveData.list = liveData.list.filter((item) => item.id !== id);
+   }
 </script>
 
 <template>
@@ -30,6 +66,24 @@
          </nav>
       </div>
    </header>
+
+   <main>
+      <ul>
+         <li>Ref</li>
+         <li v-for="item in liveDataRef" :key="item.name">
+            {{ item.name }}
+            <button @click="removeItem(item.id)">x</button>
+         </li>
+      </ul>
+
+      <ul>
+         <li>Reactive object</li>
+         <li v-for="item in liveData.list" :key="item.id">
+            {{ item.name }}
+            <button @click="removeOtherItem(item.id)">x</button>
+         </li>
+      </ul>
+   </main>
 
    <RouterView />
 </template>

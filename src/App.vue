1db6648c2c78350a8e2d5452d2cf7d6dbef7dd68
perpetual-education <admin@perpetual.education>
2023-01-20 13:06:55 -0800

    Add fullPath for stackBlitz

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,60 @@
 <script setup>
-   import { RouterLink, RouterView } from 'vue-router';
-   import HelloWorld from './components/HelloWorld.vue';
-   import ProjectLogo from './components/graphics/ProjectLogo.vue';
+	import { RouterLink, RouterView, useRoute } from 'vue-router';
+	import HelloWorld from './components/HelloWorld.vue';
+	import ProjectLogo from './components/graphics/ProjectLogo.vue';
+
+	const route = useRoute();
 </script>
 
 <template>
-   <header>
-      <div class="wrapper">
-         <nav class="site-menu">
-            <ul>
-               <li>
-                  <HelloWorld helloMessage="V3C" />
-               </li>
+	<header>
+		<div class="wrapper">
+			<pre>
+            <code>
+               {{route.fullPath}}
+            </code>  
+         </pre>
 
-               <li>
-                  <RouterLink class="home-link" to="/">
-                     <ProjectLogo />
-                  </RouterLink>
-               </li>
+			<nav class="site-menu">
+				<ul>
+					<li>
+						<HelloWorld helloMessage="V3C" />
+					</li>
 
-               <li>
-                  <RouterLink to="/">Home</RouterLink>
-               </li>
+					<li>
+						<RouterLink class="home-link" to="/">
+							<ProjectLogo />
+						</RouterLink>
+					</li>
 
-               <li>
-                  <RouterLink to="/restaurants">Restaurants</RouterLink>
-               </li>
-            </ul>
-         </nav>
-      </div>
-   </header>
+					<li>
+						<RouterLink to="/">Home</RouterLink>
+					</li>
 
-   <RouterView />
+					<li>
+						<RouterLink to="/restaurants">Restaurants</RouterLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<RouterView />
 </template>
 
 <style>
-   .site-menu ul {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      list-style: none; /* $todo */
-   }
-   .site-menu a:not(.home-link) {
-      padding: 20px;
-   }
-   .home-link {
-      display: block;
-      max-width: 30px;
-      padding: 10px;
-   }
+	.site-menu ul {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		list-style: none; /* $todo */
+	}
+	.site-menu a:not(.home-link) {
+		padding: 20px;
+	}
+	.home-link {
+		display: block;
+		max-width: 30px;
+		padding: 10px;
+	}
 </style>

131a904dd5ef3e56146134f8210bbab84837e3fb
sheriffderek <derek@sheriffderek.consulting>
2023-01-28 07:51:45 -0800

    Config unplugin version +
    - not really giving us what we want yet -

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from '@rollup/plugin-eslint';
 
+import Components from 'unplugin-vue-components/vite';
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
@@ -15,6 +17,9 @@ export default defineConfig({
 			enforce: 'pre',
 			apply: 'build',
 		},
+		Components({
+			dirs: ['src/components', 'src/views'],
+		}),
 	],
 	resolve: {
 		alias: {

97b07c9b365eb6919b2b18ebcbcf019cef8c453e
sheriffderek <derek@sheriffderek.consulting>
2023-01-28 07:51:12 -0800

    Set up example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,13 @@
+<script setup>
+	import { ref } from 'vue';
+	//import AboutView from '@/views/AboutView.vue';
+	const x = ref('hi');
+</script>
+
 <template>
 	<h1>Home page</h1>
+
+	{{ x }}
+
+	<HelloWorld />
 </template>
